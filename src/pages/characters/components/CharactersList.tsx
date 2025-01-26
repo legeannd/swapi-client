a519125634e34d94312ff081dd920f7a2c548483
Gean Lucas <gean.lfpsa@gmail.com>
2025-01-26 20:48:45 -0300

    feat: add filter to lists

--- src/pages/characters/components/CharactersList.tsx
+++ src/pages/characters/components/CharactersList.tsx
@@ -1,31 +1,47 @@
 import { Link } from "react-router";
 import CharacterAvatar from "./CharacterAvatar";
+import { useEffect, useState } from "react";
+import SearchInput from "../../../components/SearchInput";
 
 interface CharactersListProps {
-  results: [{ uid: string; name: string }];
+  results: { uid: string; name: string }[];
 }
 
 const CharactersList = ({ results }: CharactersListProps) => {
+  const [filter, setFilter] = useState("");
+  const [filteredResults, setFilteredResults] = useState(results);
+
+  useEffect(() => {
+    const newResults = results.filter((character) =>
+      character.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    setFilteredResults(newResults);
+  }, [filter, results]);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {results.map((character) => {
-        return (
-          <Link
-            to={`/characters/${character.uid}`}
-            key={character.uid}
-            className="block"
-          >
-            <div className="bg-gray-800 border-2 border-yellow-400 rounded-lg p-4 hover:bg-gray-700 transition-colors">
-              <div className="flex items-center space-x-4">
-                <CharacterAvatar name={character.name} />
-                <h2 className="text-xl font-semibold text-yellow-400">
-                  {character.name}
-                </h2>
+    <div className="flex flex-col gap-8">
+      <SearchInput type="people" onSearch={setFilter} />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {filteredResults.map((character) => {
+          return (
+            <Link
+              to={`/characters/${character.uid}`}
+              key={character.uid}
+              className="block"
+            >
+              <div className="bg-gray-800 border-2 border-yellow-400 rounded-lg p-4 hover:bg-gray-700 transition-colors">
+                <div className="flex items-center space-x-4">
+                  <CharacterAvatar name={character.name} />
+                  <h2 className="text-xl font-semibold text-yellow-400">
+                    {character.name}
+                  </h2>
+                </div>
               </div>
-            </div>
-          </Link>
-        );
-      })}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/films/components/FilmsList.tsx
+++ src/pages/films/components/FilmsList.tsx
@@ -1,42 +1,51 @@
 import { Clapperboard } from "lucide-react";
 import { Link } from "react-router";
+import SearchInput from "../../../components/SearchInput";
+import { useEffect, useState } from "react";
 
 interface FilmsListProps {
-  result: [
-    {
-      uid: string, 
-      properties: {
-        title: string,
-        director: string,
-      }
-    }
-  ];
+  result: {
+    uid: string;
+    properties: {
+      title: string;
+      director: string;
+    };
+  }[];
 }
 
 const FilmsList = ({ result }: FilmsListProps) => {
+  const [filter, setFilter] = useState("");
+  const [filteredResults, setFilteredResults] = useState(result);
+
+  useEffect(() => {
+    const newResults = result.filter((film) =>
+      film.properties.title.toLowerCase().includes(filter.toLowerCase())
+    );
+    setFilteredResults(newResults);
+  }, [filter, result]);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {result.map((film) => {
-        return (
-          <Link
-            to={`/films/${film.uid}`}
-            key={film.uid}
-            className="block"
-          >
-            <div className="flex items-center gap-4 bg-gray-800 border-2 border-yellow-400 rounded-lg p-4 hover:bg-gray-700 transition-colors">
-              <Clapperboard size={32} />
-              <div className="flex flex-col justify-start">
-                <h2 className="text-xl font-semibold text-yellow-400">
-                  {film.properties.title}
-                </h2>
-                <p className="text-sm font-semibold text-yellow-400">
-                  Directed by {film.properties.director}
-                </p>
+    <div className="flex flex-col gap-8">
+      <SearchInput type="films" onSearch={setFilter} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {filteredResults.map((film) => {
+          return (
+            <Link to={`/films/${film.uid}`} key={film.uid} className="block">
+              <div className="flex items-center gap-4 bg-gray-800 border-2 border-yellow-400 rounded-lg p-4 hover:bg-gray-700 transition-colors">
+                <Clapperboard size={32} />
+                <div className="flex flex-col justify-start">
+                  <h2 className="text-xl font-semibold text-yellow-400">
+                    {film.properties.title}
+                  </h2>
+                  <p className="text-sm font-semibold text-yellow-400">
+                    Directed by {film.properties.director}
+                  </p>
+                </div>
               </div>
-            </div>
-          </Link>
-        );
-      })}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
